6d37835f486aa5dc79155e30b7ef9dcd92caa4dd
Kyle Haptonstall <khaptonstall93@gmail.com>
2024-09-14 19:37:13 -0500

    Add tests for nested child blocks

--- Tests/Notion2MarkdownCoreTests/BlockChildrenTests.swift
+++ Tests/Notion2MarkdownCoreTests/BlockChildrenTests.swift
@@ -30,4 +30,38 @@ class BlockChildrenTests: XCTestCase {
         let expectedBlockIds = blocks.map { $0.id }.prefix(3)
         XCTAssertEqual(response.map { $0.id }, Array(expectedBlockIds))
     }
+
+    func testBlockChildrenRecursivelyFetchesNestedChildren() async throws {
+        let client = MockNotionClient()
+
+        // Setup a set of blocks that would match the following markdown:
+        /*
+         - Parent bullet 1
+            - Child bullet 1
+            - Child bullet 2
+         - Parent bullet 2
+         */
+        let parentBullet1: ReadBlock = .mocked(id: "1", type: .bulletedListItem([.mocked(string: "Parent bullet 1")]), hasChildren: true)
+        let parentBullet2: ReadBlock = .mocked(id: "2", type: .bulletedListItem([.mocked(string: "Parent bullet 2")]), hasChildren: false)
+        let childBullet1: ReadBlock = .mocked(id: "3", type: .bulletedListItem([.mocked(string: "Child bullet 1")]), hasChildren: true)
+        let childBullet2: ReadBlock = .mocked(id: "4", type: .bulletedListItem([.mocked(string: "Child bullet 2")]), hasChildren: false)
+
+        // Setup 3 mock responses:
+        // The first will fetch all the top-level blocks
+        // The second will fetch the next level of children for parentBullet1
+        // The third will fetch the next level of children for childBullet1
+        client.blockChildrenResponses = [
+            .success(.init(results: [parentBullet1, parentBullet2], nextCursor: nil, hasMore: false)),
+            .success(.init(results: [childBullet1], nextCursor: nil, hasMore: false)),
+            .success(.init(results: [childBullet2], nextCursor: nil, hasMore: false)),
+        ]
+
+        // Recursively fetch all block children
+        let response = try await client.allBlockChildren(blockId: .init(""))
+
+        // Verify the final order of blocks
+        let actualOrder = response.map { $0.id.rawValue }
+        let expectedOrder = [parentBullet1, childBullet1, childBullet2, parentBullet2].map { $0.id.rawValue }
+        XCTAssertEqual(actualOrder, expectedOrder)
+    }
 }
