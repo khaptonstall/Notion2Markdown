7106c118aaf785e7345a89e5f8aef574b3f216dd
Kyle Haptonstall <khaptonstall93@gmail.com>
2024-09-21 22:36:53 -0500

    Add helper to extract all image urls

--- Sources/Notion2MarkdownCore/Shared/Extensions/Array+ReadBlock.swift
+++ Sources/Notion2MarkdownCore/Shared/Extensions/Array+ReadBlock.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import NotionSwift
+
+extension Array where Element == ReadBlock {
+    /// Enumerates all blocks and returns each url representing a private (i.e. non-external) image file.
+    var imageURLs: [URL] {
+        compactMap { block in
+            switch block.type {
+            case .image(let value):
+                switch value.file {
+                case let .file(urlString, _):
+                    return URL(string: urlString)
+                default:
+                    return nil
+                }
+            default:
+                return nil
+            }
+        }
+    }
+}
